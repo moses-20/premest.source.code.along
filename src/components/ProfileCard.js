60611e0993cc5168485dbf8fd3f4159f8c22f133
moses-20 <mosesgameli20@gmail.com>
2022-03-18 11:54:51 +0000

    complete code along for react props and state

---
+++ src/components/ProfileCard.js
@@ -0,0 +1,21 @@
+function ProfileCard({ writer }) {
+  return (
+    <div className="card">
+      <img
+        src={`images/${writer.avatar}.png`}
+        height="300px"
+        width="300px"
+        alt={writer.img}
+      />
+      <div className="textGroup">
+        <h3>{writer.name}</h3>
+        <p>{writer.email}</p>
+        <p>{writer.phone}</p>
+
+        <button className="actionBtn"> Read Bio </button>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileCard;
