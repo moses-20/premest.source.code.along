bae53a375b353a900fb72742a319e2c20042fdba
moses-20 <mosesgameli20@gmail.com>
2022-03-25 11:14:25 +0000

    complete code along for useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import { useState } from "react";
-import ProfileForm from "./components/ProfileForm";
-import ProfileCard from "./components/ProfileCard";
+import { useEffect, useState } from "react";
+import Axios from "axios";
 
 function App() {
-  const [allProfiles, setAllProfiles] = useState([
-    {
-      firstName: "Gbemu",
-      lastName: "Terra",
-      email: "gbemu@google.com",
-      phone: "020123456"
-    }
-  ]);
+  const [posts, setPosts] = useState([]);
 
-  const updateProfiles = (profile) => {
-    let arr = allProfiles;
-    arr.push(profile);
-    setAllProfiles([...arr]);
-  };
+  useEffect(() => {
+    (async () => {
+      let response = await Axios({
+        method: "GET",
+        url: "https://jsonplaceholder.typicode.com/posts"
+      });
+
+      setPosts(response.data);
+    })();
+  });
 
   return (
     <div className="app">
       <h1> Profile Maker </h1>
       <div>
-        <ProfileForm submit={updateProfiles} />
-        <hr />
         <div className="list">
-          {allProfiles.map((person, index) => (
-            <ProfileCard key={index} card={person} />
+          {posts.map((post) => (
+            <div key={post.id} className="post">
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </div>
           ))}
         </div>
       </div>
